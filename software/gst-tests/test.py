369e990f521ae88c8ad3d7be652b4fbff64bfdd0
Eero af Heurlin <rambo@iki.fi>
2011-01-24 21:33:52 +0200

    testing tees and multiple bins, gettting
    (test.py:7001): GStreamer-WARNING **: adding flushing pad 'ghost0' to running element 'bin0', you need to use gst_pad_set_active(pad,TRUE) before adding it.

--- software/gst-tests/test.py
+++ software/gst-tests/test.py
@@ -56,11 +56,18 @@ class GTK_Main():
         #self.player = gst.parse_launch('videotestsrc name=source ! video/x-raw-yuv,format=(fourcc)AYUV ! videomixer name=mix ! ffmpegcolorspace ! videoscale ! autovideosink name=videosink')
         #self.player = gst.parse_launch('videotestsrc name=source ! videoscale ! video/x-raw-yuv,format=(fourcc)AYUV ! videomixer name=mix ! ffmpegcolorspace !  autovideosink name=videosink')
         # This hardcoded resolution works.
-        self.player = gst.parse_launch('videotestsrc name=source ! video/x-raw-yuv,format=(fourcc)AYUV,width=500,height=400 ! videomixer name=mix ! ffmpegcolorspace !  autovideosink name=videosink')
+        #self.player = gst.parse_launch('videotestsrc name=source ! video/x-raw-yuv,format=(fourcc)AYUV,width=500,height=400 ! videomixer name=mix ! ffmpegcolorspace !  autovideosink name=videosink')
+        self.player = gst.parse_launch('videotestsrc name=source ! tee name=player')
+        self.bin1 = gst.gst_parse_bin_from_description('queue ! video/x-raw-yuv,format=(fourcc)AYUV,width=500,height=400 ! videomixer name=mix ! ffmpegcolorspace !  autovideosink name=videosink', True)
+        self.player.add(self.bin1)
+        #self.bin2 = gst.gst_parse_bin_from_description('queue ! video/x-raw-yuv,format=(fourcc)AYUV,width=500,height=400 ! videomixer name=mix ! ffmpegcolorspace !  tcpserversink name=videosink', True)
+        #self.player.add(self.bin2)
+        self.tee = self.player.get_by_name("player")
+        self.tee.link(self.bin1)
 
         # adapted from https://code.fluendo.com/flumotion/trac/browser/flumotion/trunk/flumotion/component/converters/overlay/overlay.py
-        self.videomixer = self.player.get_by_name("mix")
-        self.converter = self.player.get_by_name("conv")
+        self.videomixer = self.bin1.get_by_name("mix")
+        self.converter = self.bin1.get_by_name("conv")
         self.videomixer.get_pad('sink_0').connect('notify::caps', self._notify_caps_cb)
         self.sourceBin = gst.Bin()
         self.overlay = gst.element_factory_make('appsrc', 'overlay')
